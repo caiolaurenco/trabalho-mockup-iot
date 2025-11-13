8479791235d6f63a49967b0371ce553dd325e43a
caiolaurenco <caio_laurenco@estudante.sesisenai.org.br>
2025-11-13 10:12:02 -0300

    terminei de configurar o sensor de iluminação

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,45 +1,69 @@
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include "env.h"
 
-WiFiClient client;
+WiFiClientSecure client;
 PubSubClient mqtt(client);
 
-const String BrokerURL = "test.mosquitto.org";
-const int BrokerPort = 1883;
 
-const String SSID = "FIESC_IOT_EDU";
-const String PASS = "8120gv08";
-const String BrokerUser = "";
-const String BrokerPass = "";
+const byte ldr = 34;
+int threshold = 0;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Conectando ao WiFi");
-  WiFi.begin(SSID,PASS);
-  while(WiFi.status() != WL_CONNECTED){
+  pinMode(ldr, INPUT);
+  pinMode(19, OUTPUT);
+  threshold = analogRead(ldr);
+  client.setInsecure();
+  Serial.println("Conectando no WiFi");
+  WiFi.begin(WIFI_SSID, WIFI_PASS); 
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println(".");
+    delay(200);
+  }
+  Serial.println("\nConectado com Sucesso no WiFi!");
+
+  Serial.println("Conectando ao Broker...");
+  mqtt.setServer(BROKER_URL, BROKER_PORT);
+  String BoardID = "S1";
+  BoardID += String(random(0xffff), HEX);
+  mqtt.connect(BoardID.c_str(), BROKER_USER, BROKER_PASS);
+  while (!mqtt.connected()) {
     Serial.print(".");
     delay(200);
   }
-  Serial.println("\nConectado com Sucesso");
-  
-  Serial.println("conectando ao Broker...");
-  mqtt.setServer(BrokerURL.c_str(),BrokerPort);
-  String BoardID = "S1";
-  BoardID += String(random(0xffff),HEX);
-  mqtt.connect(BoardID.c_str(),BrokerUser.c_str(),BrokerPass.c_str());
-  while(!mqtt.connected()){
-    Serial.print(".");
-    delay(200);
+  mqtt.subscribe(TOPIC_ILUM);
+  mqtt.setCallback(callback);  
+  Serial.println("\nConectado ao Broker!");
+  Serial.println(threshold);
+}
 
+void loop() {
+  int valor_ldr = analogRead(ldr);
+  // Serial.println(valor_ldr);
+  if ((valor_ldr > threshold + 100)||(valor_ldr < threshold - 100)) {
+    mqtt.publish(TOPIC_ILUM, "Claro"); 
+    mqtt.loop();
+    delay(2000);
+  } else {
+    mqtt.publish(TOPIC_ILUM, "Escuro"); 
+    mqtt.loop();
+    delay(2000);
   }
 }
 
-
-void loop() {
-  String mensagem = "Caio: ";
-  mensagem += "oi";
-  mqtt.publish("Topico-DSM14", mensagem.c_str());
-  mqtt.loop();
-  delay(1000);
-
+void callback(char* topic, byte* payload, unsigned int length) {
+  String msg = "";
+  for (int i = 0; i < length; i++) {
+    msg += (char)payload[i];
+  }
+  msg = msg.c_str();
+  if (strcmp(topic,TOPIC_ILUM) == 0 && msg == "Claro") {  
+    Serial.println(msg);
+    digitalWrite(19, LOW);
+  } else if (strcmp(topic,TOPIC_ILUM)==0 && msg == "Escuro") { 
+    digitalWrite(19, HIGH);
+    Serial.println(msg);
+  }
 }
